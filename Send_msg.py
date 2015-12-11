10eef8fc9f288d2a3dc95b8dedc47a146a879db5
rishabhjain2018 <rishabhjain2018@gmail.com>
2015-12-11 23:52:24 +0530

    Updated Old Scripts

--- Send_msg.py
+++ Send_msg.py
@@ -3,5 +3,8 @@ from twilio.rest import TwilioRestClient
 account_sid= "AC862fd8efe5524ed2464e60b064d3f750" 
 auth_token= "cd8b6d980a9323c1e5a743f87899be0a"
 client = TwilioRestClient(account_sid,auth_token)
-message = client.messages.create(to="+918375867839",from_="+16464938392",body="Hello there!!")
+body=str(raw_input("Enter the message to be delivered:"))
+for i in range(0,1):
+	message = client.messages.create(to="+918375867839",from_="+16464938392",body=body)
+	mess=client.messages.create(to="+918375867839",from_="+16464938392",body="hello world")
 print message.sid
---
+++ web.py
@@ -0,0 +1,2 @@
+import webbrowser
+webbrowser.open("http://192.168.0.1/index.htm")
