cbb71b6b6e3a8b8c9c882a2012aaf30925377d23
RishabhJain2018 <rishabhjain2018@gmail.com>
2015-12-30 00:18:56 +0530

    Updated Youtube.py

--- Youtube.py
+++ Youtube.py
@@ -8,3 +8,12 @@ choice = raw_input("Input d to Download or ei to Extract Information")
 if choice == 'd':
 	best=video.getbest()
 	best.download(quiet=False)
+elif choice == 'ei':
+	print "Title : " +video.Title
+	print "Rating : " +str(video.rating) 
+	print "View Count : " + str(video.viewcount) + "Author : " + video.author + "Length :" + str(video.length)
+	print "Duration :" + str(video.duration) + "Likes : "+ str(video.likes) + "Dislikes : "+ str(video.dislikes)
+	print "Description : "+ video.description
+	print "Published on Date : " + str(video.published)
+else:
+	print "Oops wrong Choice !"
