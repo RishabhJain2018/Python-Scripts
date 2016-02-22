c553e1c5f4533f8fae5d50c62967e94e4fc00984
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-02-23 01:02:59 +0530

    Modified Weather Script

--- Weather.py
+++ Weather.py
@@ -1,20 +1,12 @@
 import urllib2
 import re
 import json
-from urllib2 import urlopen
 
-def getPublicIP():
-	data= str(urlopen('https://checkip.dyndns.com/').read())
-	return re.compile(r'Address : (\d+.\d+.\d+.\d+)').search(data).group(1)
 
-IP=str(getPublicIP)
 
-url = 'https://info.io/' + IP + '/json'
-response=urlopen(url)
-data=json.load(response)
-city=data['city']
+city=str(raw_input("Enter The Name of the City :  "))
 
-response = urllib2.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' + city)
+response = urllib2.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=4a87519db6e540f39c6651e9e9f89a8b')
 data= json.load(response)
 print "Your City : " + city
 print "Current Weather : "+ str(data['main']['temp']-273)+ "C"
