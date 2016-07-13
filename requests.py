bac710f38dd177ad9f165dfa1330e1d137bbd3ed
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-07-14 02:28:02 +0530

    Completed Script

---
+++ requests.py
@@ -0,0 +1,50 @@
+import urllib2
+import urllib
+import csv
+from random import randrange
+import random
+
+
+domain=['gmail', 'yahoo', 'hotmail', 'rediffmail']
+
+with open('final.csv', 'rb') as csvfile:
+
+	for line in csvfile.readlines():
+		names = line.split(',')
+		
+
+
+for i in range(0,100):
+	a=str(randrange(7,10))
+	b=str(randrange(7,10))
+	c=str(randrange(70,100))
+	d=str(randrange(111111, 999999))
+
+	# print a+b+c+d
+	name = names[i]
+	phone_no = a+b+c+d	
+	email = names[i].lower()+str(randrange(1111, 9999))+"@"+random.choice(domain)+".com"
+	values = {'name': name, 'email':email, 'phone_no':phone_no}
+
+	try:
+		url = "http://www.jbitdoon.com/index.php"
+		data = urllib.urlencode(values)
+		req = urllib2.Request(url, data)
+		response= urllib2.urlopen(req)
+		print response.getcode()
+	except urllib2.HTTPError as e:
+		if e.code == 404:
+			print "Error 404"
+		else:
+			body=response.read()
+			print body
+
+
+
+
+
+
+
+		
+
+		
